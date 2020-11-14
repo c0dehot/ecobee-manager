ca22c82d08fac9698fc2fcd3a700c63986e379ef
C0dehot <fil@codehot.tech>
2020-11-14 14:16:30 -0500

    Added debug code to ecobee

--- ecobee.js
+++ ecobee.js
@@ -1,6 +1,6 @@
 /*************************************************
  * Written 18 Jan, 2020 by Filipe Laborde
- * Updated 4 Nov, 2020
+ * Updated 11 Nov, 2020
  * 
  * Open-source, MIT license - use as you wish.
  * 
@@ -8,50 +8,53 @@
  * npm install --save tplink-cloud-api nodemailer axios uuid dotenv
  * 
  * Configure .env file with elements below in SETTINGS
+ * 
+ * Understanding ecobee login system: https://www.ecobee.com/home/developer/api/examples/ex1.shtml
  */
 
-const result = require('dotenv').config();
+const result = require('dotenv').config()
 if( result.error ){
-  console.log( `ERROR: Missing .env, please create it!` ); //result.error
-  process.exit(1);
+  console.log( `ERROR: Missing .env, please create it!` ) //result.error
+  process.exit(1)
 }
 
-const fs = require('fs');
-const nodemailer = require('nodemailer');
-const axios = require('axios').default;
-const { login } = require("tplink-cloud-api");
-const uuidV4 = require("uuid/v4");
+const fs = require('fs')
+const nodemailer = require('nodemailer')
+const axios = require('axios').default
+const { login } = require("tplink-cloud-api")
+const uuidV4 = require("uuid/v4")
 
 /* SETTINGS (from .env file) ------ read sensor data and alert when it's out of scope */
-const API_KEY       = process.env.API_KEY;
-const ALERTS_EMAIL  = process.env.ALERTS_EMAIL;
-const SMTP_LOGIN    = process.env.SMTP_LOGIN;
-const SMTP_PASS     = process.env.SMTP_PASS;
-const SETTINGS_FILE = __dirname + process.env.SETTINGS_FILE;
-const LOG_FILE      = __dirname + process.env.LOG_FILE;
-const ERROR_FILE    = __dirname + process.env.ERROR_FILE;
-const TPLINK_USER   = process.env.TPLINK_USER;
-const TPLINK_PASS   = process.env.TPLINK_PASS;
-const TPLINK_TERM   = uuidV4();
+const API_KEY       = process.env.API_KEY
+const ALERTS_EMAIL  = process.env.ALERTS_EMAIL
+const SMTP_LOGIN    = process.env.SMTP_LOGIN
+const SMTP_PASS     = process.env.SMTP_PASS
+const SETTINGS_FILE = __dirname + process.env.SETTINGS_FILE
+const LOG_FILE      = __dirname + process.env.LOG_FILE
+const ERROR_FILE    = __dirname + process.env.ERROR_FILE
+const TPLINK_USER   = process.env.TPLINK_USER
+const TPLINK_PASS   = process.env.TPLINK_PASS
+const TPLINK_TERM   = uuidV4()
+const DEBUG         = process.env.DEBUG || 0
 
 function logWrite( output, type='' ){
-    fs.appendFileSync( LOG_FILE, output+"\n" );
+    fs.appendFileSync( LOG_FILE, output+"\n" )
 
     // no notification, don't bother console.logging (that generates an email notify)
-    if( type.indexOf('notify') === -1 ) return;
+    if( type.indexOf('notify') === -1 ) return
 
     // check when last error happened, if it was within 75 minutes, then let's send message
-    let lastTimestamp = Number(fs.existsSync(ERROR_FILE) ? fs.readFileSync( ERROR_FILE ) : 0);
-    const NOW = Date.now() / 1000 / 60;
+    let lastTimestamp = Number(fs.existsSync(ERROR_FILE) ? fs.readFileSync( ERROR_FILE ) : 0)
+    const NOW = Date.now() / 1000 / 60
     if( type === 'always-notify' || (NOW-lastTimestamp)>75 ){    
-        console.log( output );
+        console.log( output )
     }
     // update error notify timestamp
-    fs.writeFileSync( ERROR_FILE, NOW );
+    fs.writeFileSync( ERROR_FILE, NOW )
 }
 
 function settingsSave( settings={} ){
-    fs.writeFileSync( SETTINGS_FILE, JSON.stringify(settings) );
+    fs.writeFileSync( SETTINGS_FILE, JSON.stringify(settings) )
 }
 
 function mailSend( subject, text ){
@@ -62,110 +65,118 @@ function mailSend( subject, text ){
             user: SMTP_LOGIN,
             pass: SMTP_PASS
             }
-        });
+        })
 
         // Message object
         let message = {
             // Comma separated list of recipients
             to: `Alert <${ALERTS_EMAIL}>`,
             subject, text
-        };
+        }
 
+        if( DEBUG ) console.log( `Sending email...` )
         transporter.sendMail(message, (error, info) => {
             if (error) {
-                console.log( 'Error occurred: '+ error.message );
-                reject();
+                console.log( 'Error occurred: '+ error.message )
+                reject()
             }
 
-            console.log('Message sent successfully!');
-            transporter.close();
-            resolve();
-        });
-    });
+            console.log('Message sent successfully!')
+            transporter.close()
+            resolve()
+        })
+    })
 }
 
 async function deviceToggle( name, powerMode ){
     // log in to cloud, return a connected tplink object
-    const tplink = await login(TPLINK_USER, TPLINK_PASS, TPLINK_TERM);
+    const tplink = await login(TPLINK_USER, TPLINK_PASS, TPLINK_TERM)
     
     // get a list of raw json objects (must be invoked before .get* works)
-    const dl = await tplink.getDeviceList();
+    const dl = await tplink.getDeviceList()
     
-    const device = tplink.getHS110(name); 
-    const response = await (powerMode=='powerOn' ? device.powerOn() : device.powerOff() );
+    const device = tplink.getHS110(name)
+    const response = await (powerMode=='powerOn' ? device.powerOn() : device.powerOff() )
     
-    return response.system.set_relay_state.err_code == 0;
+    return response.system.set_relay_state.err_code == 0
 }
 
 async function deviceCheck( name ){
     // log in to cloud, return a connected tplink object
-    const tplink = await login(TPLINK_USER, TPLINK_PASS, TPLINK_TERM);
+    const tplink = await login(TPLINK_USER, TPLINK_PASS, TPLINK_TERM)
     
     // get a list of raw json objects (must be invoked before .get* works)
-    const dl = await tplink.getDeviceList();
+    const dl = await tplink.getDeviceList()
     
-    const device = tplink.getHS110(name); 
+    const device = tplink.getHS110(name)
     return device.getRelayState()
 }
 
 
 async function appRun( retryOnError=true ) {
-    const LAST_RETRY = false;
-    let settings = {};
+    const LAST_RETRY = false
+    let settings = {}
+    if( DEBUG ) console.log( `[appRun] starting` )
     try { 
-        settings = JSON.parse( fs.readFileSync( SETTINGS_FILE ) );
+        settings = JSON.parse( fs.readFileSync( SETTINGS_FILE ) )
     } catch( e ){
-        console.log( `~ error reading settings, creating new file.` );
-        settingsSave();
+        console.log( `~ error reading settings, creating new file.` )
+        settingsSave()
     }
 
     // -- ADDING APP (with PIN method) ----------------------
     // code is only run first time ecobee app added
     if( !settings.authCode ){
         // now we generate 4-digit PIN that the user must use to Add to 'MyApps' with permissions
-        const response = await axios.get(`https://api.ecobee.com/authorize?response_type=ecobeePin&client_id=${API_KEY}&scope=smartRead`)
-
-        const pin = response.data.ecobeePin;
-        settings.authCode = response.data.code;
+        const url = `https://api.ecobee.com/authorize?response_type=ecobeePin&client_id=${API_KEY}&scope=smartRead`
+        if( DEBUG ) console.log( ` .. getting authCode, url: ${url}` )
+        const response = await axios.get(url)
+        if( DEBUG ) console.log( ` ... response: `, response )
+        const pin = response.data.ecobeePin
+        settings.authCode = response.data.code
+        if( DEBUG ) console.log( ` .... pin(${pin}) authCode(${settings.authCode})` )
 
         console.log( `
         ** User Action Required **
         Go to 'My Apps' > 'Add Application' and enter this PIN (and add the app): ${pin}      
-        `);
+        `)
 
-        settingsSave(settings);
-        const mailResponse = await mailSend( '!App Auth Required', 'You need to relogin to the Ecobee app' );
+        settingsSave(settings)
+        const mailResponse = await mailSend( '!App Auth Required', 'You need to relogin to the Ecobee app' )
 
         // exit, let user add the app
-        process.exit(1);
+        process.exit(1)
     }
 
     // request an access token (needed once an hour when token expires)
     // we use the ecobee PIN above first time, then that gives us a 'refresh_token' that we use going forward
     if( !settings.access_token ){
         // attempt to use refresh_token (as the actual token expires after 1 hour)
-        const response = await axios.post( `https://api.ecobee.com/token`,
-                `client_id=${API_KEY}&` +( settings.refresh_token ? 
-                    `grant_type=refresh_token&code=${settings.refresh_token}` :
-                    `grant_type=ecobeePin&code=${settings.authCode}` ) );
-                
+        const url = `https://api.ecobee.com/token`
+        const data = `client_id=${API_KEY}&` +( settings.refresh_token ? 
+            `grant_type=refresh_token&code=${settings.refresh_token}` :
+            `grant_type=ecobeePin&code=${settings.authCode}` )
+        if( DEBUG ) console.log( ` .. getting access_token, url: ${url} / ${data}` )
+        const response = await axios.post( url, data )
+        if( DEBUG ) console.log( ` ... response: `, response )
+
         if( response.data.error ){
-            logWrite( `~ ERROR: {$jsonResult['error_description']}`, 'notify' );
+            logWrite( `~ ERROR: {$jsonResult['error_description']}`, 'notify' )
 
             if( response.data.error=='authorization_expired' )
-                delete( settings.authCode );
+                delete( settings.authCode )
             else
-                delete( settings.access_token );
-            settingsSave(settings);  
+                delete( settings.access_token )
+            settingsSave(settings)
 
             // let's recursively retry ONCE (second recursion is retryOnError=false)
-            if( retryOnError ) appRun(LAST_RETRY);
-            process.exit(1);
+            if( retryOnError ) appRun(LAST_RETRY)
+            process.exit(1)
         }
 
         // update settings with latest token info
-        settings = { ...settings, ...response.data };
-        settingsSave( settings );
+        settings = { ...settings, ...response.data }
+        settingsSave( settings )
     }
 
 
@@ -177,70 +188,70 @@ async function appRun( retryOnError=true ) {
             {   selectionType: "registered",
                 selectionMatch: "",
                 includeSensors: "true",
-                includeRuntime: "true" } });
+                includeRuntime: "true" } })
     // console.log( `.. about to get url: header Auth(${settings.token_type} ${settings.access_token}): `, url );   
-    let response, statusCode;
+    let response, statusCode
     try {
         response = await axios.get( url, {
                 headers: {
                 "Content-Type": "text/json",
                 Authorization: `${settings.token_type} ${settings.access_token}`
                 }
-            });
+            })
 
-        statusCode = response.data.status.code;
+        statusCode = response.data.status.code
 
     } catch( error ){
         // Error 500 happens when auth expired
         // But the actual response is still passed back (axios puts in error.response), so we 
         // use that and proceed accordingly.
-        logWrite( `~ API-Call-Error: ${url} message: `+error.message, 'notify' );
+        logWrite( `~ API-Call-Error: ${url} message: `+error.message, 'notify' )
 
-        response = error.response;
+        response = error.response
     }
 
     if( statusCode !== 0 ){
         logWrite( `~ API-Error: ${response.data.status.message} `
-        + (statusCode == 14 ? `; (stale access_token)` : '' ), 'notify' );
+        + (statusCode == 14 ? `; (stale access_token)` : '' ), 'notify' )
 
         // expired auth token, clearing so we get another
-        delete( settings.access_token );
-        settingsSave( settings );
+        delete( settings.access_token )
+        settingsSave( settings )
 
         // retry with cleared access token, and fail after that if still errors
-        if( retryOnError ) appRun(LAST_RETRY);
-        process.exit(1);
+        if( retryOnError ) appRun(LAST_RETRY)
+        process.exit(1)
     }
 
-    logWrite( "\n" + new Date().toLocaleString() );
+    logWrite( "\n" + new Date().toLocaleString() )
 
     response.data.thermostatList.forEach( thermostat => {
         if( !thermostat.runtime.connected ){
-            logWrite( `\t! Thermostat NOT connected currently: ${thermostat.name}` );
-            return;
+            logWrite( `\t! Thermostat NOT connected currently: ${thermostat.name}` )
+            return
         }
 
-        const runTime = thermostat.runtime;
-        const temp = ((Number(runTime.actualTemperature) - 320) * 5/90).toFixed(1);
-        const humidity = runTime.actualHumidity;
-        logWrite( `\t${thermostat.name}: Overall temp: ${temp}℃, ${humidity}%` );
+        const runTime = thermostat.runtime
+        const temp = ((Number(runTime.actualTemperature) - 320) * 5/90).toFixed(1)
+        const humidity = runTime.actualHumidity
+        logWrite( `\t${thermostat.name}: Overall temp: ${temp}℃, ${humidity}%` )
             
         thermostat.remoteSensors.forEach( async remoteSensor => {
-            let temp = 0; let occupancy = false; let sensorPlug = ''; let sensorWarning = '';
+            let temp = 0; let occupancy = false; let sensorPlug = ''; let sensorWarning = ''
             remoteSensor.capability.forEach( capability => {
                 if( capability.type=='temperature' )
-                    temp = ( (Number(capability.value)-320) * 5/90 ).toFixed(1);
+                    temp = ( (Number(capability.value)-320) * 5/90 ).toFixed(1)
                 else if( capability.type=='occupancy' )
-                    occupancy = capability.value=="true";
+                    occupancy = capability.value=="true"
             })
 
             // Monitor temperature for some rooms and alert
             if( temp<5 && ['Sunroom','Downstairs'].find( name=>name===remoteSensor.name ) ){
-                sensorWarning = '!!(temp critical)';
+                sensorWarning = '!!(temp critical)'
                 const mailResponse = await mailSend( 
                     `!${remoteSensor.name} Temperature ${temp} degrees`, 
-                    `${remoteSensor.name} temperature low -> action required` );
-                logWrite( `\t\t * WARNING ${remoteSensor.name} temperature critical, sending email!\n`, 'always-send' );
+                    `${remoteSensor.name} temperature low -> action required` )
+                logWrite( `\t\t * WARNING ${remoteSensor.name} temperature critical, sending email!\n`, 'always-send' )
                 // send a text message too?
             }
 
@@ -248,44 +259,44 @@ async function appRun( retryOnError=true ) {
             // currently it monitors one sensor called 'Sunroom' and if it's below 11 degrees, 
             // it will toggle a TP-Link switch called 'SunroomHeater', and turn off when above 12 degrees.
             if( remoteSensor.name=='Sunroom' ){
-                let powerMode;
+                let powerMode
                 // every hour check the actual device status to make sure it's matching our settings copy
                 if( (new Date().getMinutes())<10 ){
                     const verifyPowerMode = await deviceCheck( 'SunroomHeater' )
                     if( settings.device_SunroomHeater === 'powerOff' && verifyPowerMode==1 ){
                         // problems, device marked as off, yet check indicated it was on
-                        settings.device_SunroomHeater = 'powerOn';
-                        logWrite( `\t[deviceAction] !Error: SunroomHeater *ERROR* indicated as 'powerOff' but relay was on, indicating ON`, 'always-notify' );
+                        settings.device_SunroomHeater = 'powerOn'
+                        logWrite( `\t[deviceAction] !Error: SunroomHeater *ERROR* indicated as 'powerOff' but relay was on, indicating ON`, 'always-notify' )
 
                     } else if( settings.device_SunroomHeater === 'powerOn' && verifyPowerMode==0 ){
-                        settings.device_SunroomHeater = 'powerOff';
-                        logWrite( `\t[deviceAction] !Error: SunroomHeater *ERROR* indicated as 'powerOn' but relay was off, indicating OFF`, 'always-notify' );
+                        settings.device_SunroomHeater = 'powerOff'
+                        logWrite( `\t[deviceAction] !Error: SunroomHeater *ERROR* indicated as 'powerOn' but relay was off, indicating OFF`, 'always-notify' )
                     }
                 } 
 
                 if( temp>12 && settings.device_SunroomHeater !== 'powerOff' )
-                    powerMode = 'powerOff';
+                    powerMode = 'powerOff'
                 else if( temp<11 && settings.device_SunroomHeater !== 'powerOn' )
-                    powerMode = 'powerOn';
+                    powerMode = 'powerOn'
                 
                 if( powerMode ){
-                    const result = await deviceToggle( 'SunroomHeater', powerMode );
+                    const result = await deviceToggle( 'SunroomHeater', powerMode )
                     if( result ){
-                        settings.device_SunroomHeater = powerMode;
-                        settingsSave(settings);
-                        logWrite( `\t[deviceAction] SunroomHeater ${powerMode} (temp=${temp})` );
+                        settings.device_SunroomHeater = powerMode
+                        settingsSave(settings)
+                        logWrite( `\t[deviceAction] SunroomHeater ${powerMode} (temp=${temp})` )
                     } else {
-                        logWrite( `\t[deviceAction] !Error: SunroomHeater *FAILED* ${powerMode} (temp=${temp}) `, 'always-notify' );
+                        logWrite( `\t[deviceAction] !Error: SunroomHeater *FAILED* ${powerMode} (temp=${temp}) `, 'always-notify' )
                     }
                 }
 
                 // plugMode will indicate if heater on
                 sensorPlug = settings.device_SunroomHeater === 'powerOn' ? '[+] ' : ''
             }
-            logWrite(`\t\t - ${remoteSensor.name}: ${temp}℃ ${sensorPlug}${occupancy?'[*]':''} ${sensorWarning}` );
+            logWrite(`\t\t - ${remoteSensor.name}: ${temp}℃ ${sensorPlug}${occupancy?'[*]':''} ${sensorWarning}` )
             
         })
     })
 }
 
-appRun();
+appRun()
